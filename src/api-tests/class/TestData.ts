3953a1bb397e1cce871c2f1a70477b5b1479447d
Thefaceless1 <zrjdif93@gmail.com>
2022-12-20 14:51:16 +0300

    поправлены существующие тесты. добавлены тесты для заявок

--- src/api-tests/class/TestData.ts
+++ src/api-tests/class/TestData.ts
@@ -2,19 +2,25 @@ import * as fs from "fs";
 import {RequestProp} from "./RequestProp";
 import {randomInt} from "crypto";
 
+
 export class TestData {
     public static fileInfo: [string, string][] = [];
     public static readonly fileNames = fs.readdirSync(RequestProp.fileDir);
     public static readonly dateNow: string = new Date().toLocaleDateString().split(".").reverse().join("-");
     public static readonly dateFuture: string = new Date(Date.now() + 5000000000).toLocaleDateString().split(".").reverse().join("-");
+    public static readonly commentValue : string = "Тестовый комментарий";
+    public static readonly descValue : string = "Тестовое описание";
     public static seasons: TCurrentSeason[] = [];
     public static criteriaGroups: TCriteriaGroups[] = [];
     public static licTypes: TLicTypes[] = [];
     public static docTypes: TDocTypes[] = [];
     public static rankCriteria : TRankCriteria[] =[];
     public static criteriaTypes : TCriteriaTypes[] =[];
+    public static licStatus : TLicAndDocStatus[] =[];
+    public static docStatus : TLicAndDocStatus[] =[];
     public static criteriasFullInfo : TCriteriaFullInfo[] =[];
     public static prolicense : TProlicense[] = [];
+    public static license : TLicense[] =[];
 
     public static getRandomWord(): string {
         const alphabet: string = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
@@ -24,6 +30,44 @@ export class TestData {
 
         }
         return randomWord;
+    }
+    public static getLicStatusById(stateId : number) : string {
+        const result  = TestData.licStatus.find(value => value.id == stateId);
+        return (result) ? result.name : "Статус по id не найден";
+    }
+    public static getDocStatusById(docStateId : number) : string {
+        const result = TestData.docStatus.find(value => value.id == docStateId);
+        return (result) ? result.name : "Статус по id не найден";
+    }
+    public static addClubWorkersToCritGrp () {
+        TestData.license[0].criteriaGroups.forEach((value, index) => {
+            value.experts = [0,1,2];
+        })
+        return TestData.license[0].criteriaGroups;
+    }
+    public static addDocAndComToCritDoc () {
+        TestData.license[0].criteriaGroups.forEach((value, index) => {
+            value.criterias.forEach((value1, index1) => {
+                value1.documents.forEach((value2, index2) => {
+                    value2.comment = TestData.commentValue;
+                    value2.files = [
+                        {
+                            name: TestData.fileInfo[0][0],
+                            storageId: TestData.fileInfo[0][1]
+                        },
+                        {
+                            name: TestData.fileInfo[1][0],
+                            storageId: TestData.fileInfo[1][1]
+                        },
+                        {
+                            name: TestData.fileInfo[2][0],
+                            storageId: TestData.fileInfo[2][1]
+                        }
+                    ]
+                })
+            })
+        })
+        return TestData.license[0];
     }
 
 }
@@ -60,6 +104,12 @@ export type TCriteriaTypes = {
     name : string,
     description : string
 }
+export type TLicAndDocStatus = {
+    id: number,
+    name: string,
+    description: string
+}
+
 export type TProlicense = {
     id : number
     type: string
@@ -104,5 +154,96 @@ export type TCriteriaFullInfo = {
         isMulti: number,
         typeId: number,
         documents: TDocuments[]
+    }[]
+}
+
+export type TLicense = {
+    id: number,
+    proLicId: number,
+    type: string,
+    season: string,
+    name: string,
+    begin: string,
+    end: string,
+    clubId: number,
+    requestBegin: string,
+    requestEnd: string,
+    docSubmitDate: string,
+    dueDate: string,
+    reviewDate: string,
+    decisionDate: string,
+    percent: number,
+    stateId: number,
+    state: string,
+    docStateId: number,
+    docState: string,
+    documents: {
+        id: number,
+        proDocId: number,
+        name: string,
+        docTypeId: number,
+        comment: string,
+        reviewComment: string,
+        stateId: number,
+        state: string,
+        templates: {
+            id: number,
+            name: string,
+            storageId: string
+        }[],
+        files : {
+            id: number,
+            name: string,
+            storageId: string
+        }[]
+    }[],
+    criteriaGroups : {
+        groupId : number,
+        name : string,
+        percent : number,
+        stateId : number,
+        state : string,
+        experts : number[],
+        rfuExpertChoice : number[],
+        rfuExpert : number,
+        criterias : {
+            id: number,
+            proCritId: number,
+            number: string,
+            categoryId: number,
+            name: string,
+            description: string,
+            isMulti: number,
+            typeId: number,
+            orderNum: number,
+            docSubmitDate: string,
+            reviewDate: string,
+            externalId: number,
+            comment: string,
+            percent: number,
+            stateId: number,
+            state: string,
+            documents: {
+                id: number,
+                proDocId: number,
+                name: string,
+                docTypeId: number,
+                comment: string,
+                reviewComment: string,
+                externalId : number,
+                stateId: number,
+                state: string,
+                templates: {
+                    id: number,
+                    name: string,
+                    storageId: string
+                }[],
+                files : {
+                    id?: number,
+                    name: string,
+                    storageId: string
+                }[]
+            }[]
+        }[]
     }[]
 }
--- src/api-tests/constructor.test.ts
+++ src/api-tests/constructor.test.ts
@@ -1,7 +1,7 @@
 import {jest, test, expect, describe, beforeAll} from "@jest/globals";
 import superagent from "superagent";
 import {RequestProp} from "./class/RequestProp";
-import {TestData, TCurrentSeason, TCriteriaFullInfo, TDocuments} from "./class/TestData";
+import {TestData, TCurrentSeason, TCriteriaFullInfo, TDocuments, TLicense} from "./class/TestData";
 import {randomInt} from "crypto";
 
 
@@ -16,25 +16,29 @@ describe("Создание тестовых данных", () => {
             const response = await superagent.post(RequestProp.basicUrl + "/api/rest/uploadFile").
             set("Content-Type", "multipart/form-data; boundary=----WebKitFormBoundaryoI4AK63JZr8jUhAa").
             attach("file", RequestProp.fileDir + i);
-
+            //Проверяем статус ответа и наличие возвращаемого значения в data
             expect(response.ok).toBeTruthy();
             expect(response.status).toBe(200);
             expect(response.body.status).toBe("SUCCESS");
             expect(response.body.data).toBeDefined();
+            //Записываем полученные id файлов  в свойство fileInfo
             TestData.fileInfo.push([i, response.body.data]);
         }
 
     })
     test("Справочник сезонов", async () => {
         const response = await superagent.get(RequestProp.basicUrl + "/api/rest/seasons");
+        //проверяем статус ответа и длинну массива data
         expect(response.ok).toBeTruthy();
         expect(response.status).toBe(200);
         expect(response.body.data.length).toBeGreaterThan(0);
+        //Записываем полученные значения текущего сезона и последнего сезона справочника в свойство seasons
         TestData.seasons = response.body.data.filter((value: TCurrentSeason, index: number, arr: TCurrentSeason[]) => (value.current || index == arr.length - 1));
     })
 
     test("Справочник Группы критериев", async () => {
         const response = await superagent.get(RequestProp.basicUrl + "/api/rest/prolicenses/criterias/groups");
+        //проверяем статус ответа и длинну массива data
         expect(response.ok).toBeTruthy();
         expect(response.status).toBe(200);
         expect(response.body.data.length).toBeGreaterThan(0);
@@ -42,6 +46,7 @@ describe("Создание тестовых данных", () => {
     })
     test("Справочник Типы лицензий", async () => {
         const response = await superagent.get(RequestProp.basicUrl + "/api/rest/lictypes");
+        //проверяем статус ответа и длинну массива data
         expect(response.ok).toBeTruthy();
         expect(response.status).toBe(200);
         expect(response.body.data.length).toBeGreaterThan(0)
@@ -50,6 +55,7 @@ describe("Создание тестовых данных", () => {
     })
     test("Справочник Типы документов", async () => {
         const response = await superagent.get(RequestProp.basicUrl + "/api/rest/doctypes");
+        //проверяем статус ответа и длинну массива data
         expect(response.ok).toBeTruthy();
         expect(response.status).toBe(200);
         expect(response.body.data.length).toBeGreaterThan(0);
@@ -57,6 +63,7 @@ describe("Создание тестовых данных", () => {
     })
     test("Разряды для критериев", async () => {
         const response = await superagent.get(RequestProp.basicUrl+"/api/rest/prolicenses/criterias/categories");
+        //проверяем статус ответа и длинну массива data
         expect(response.ok).toBeTruthy();
         expect(response.status).toBe(200);
         expect(response.body.data.length).toBeGreaterThan(0);
@@ -64,17 +71,37 @@ describe("Создание тестовых данных", () => {
     })
     test("Типы критериев",async () => {
         const response = await superagent.get(RequestProp.basicUrl+"/api/rest/prolicenses/criterias/types");
+        //проверяем статус ответа и длинну массива data
         expect(response.ok).toBeTruthy();
         expect(response.status).toBe(200);
         expect(response.body.data.length).toBeGreaterThan(0);
         TestData.criteriaTypes = response.body.data;
     })
+    test("Возможные статусы заявки",async () => {
+        const response = await superagent.get(RequestProp.basicUrl+"/api/rest/licenses/states");
+        //проверяем статус ответа и длинну массива data
+        expect(response.ok).toBeTruthy();
+        expect(response.status).toBe(200);
+        expect(response.body.data.length).toBeGreaterThan(0);
+        //Записываем полученные значения справочника в свойство licStatus
+        TestData.licStatus = response.body.data
+    })
+    test("Возможные статусы документов",async () => {
+        const response = await superagent.get(RequestProp.basicUrl+"/api/rest/licenses/docstates");
+        //проверяем статус ответа и длинну массива data
+        expect(response.ok).toBeTruthy();
+        expect(response.status).toBe(200);
+        expect(response.body.data.length).toBeGreaterThan(0);
+        //Записываем полученные значения справочника в свойство docStatus
+        TestData.docStatus = response.body.data;
+    })
 })
 
-describe("Конструктор пролицензий", () => {
+describe("Пролицензия", () => {
 
     test("Создание проекта лицензии", async () => {
-        const response = await superagent.put(RequestProp.basicUrl + "/api/rest/prolicenses").send({
+        const response = await superagent.put(RequestProp.basicUrl + "/api/rest/prolicenses").
+        send({
                 "name": TestData.getRandomWord(),
                 "season": TestData.seasons[0].name,
                 "type": TestData.licTypes[0].name,
@@ -123,7 +150,9 @@ describe("Конструктор пролицензий", () => {
     })
 
     test("Изменение проекта лицензии", async () => {
-        const response = await superagent.put(RequestProp.basicUrl + "/api/rest/prolicenses/" + TestData.prolicense[0].id).send({
+        //Меняем наименование, тип , сезон пролицензии и добавляем второй документ
+        const response = await superagent.put(RequestProp.basicUrl + "/api/rest/prolicenses/" + TestData.prolicense[0].id).
+        send({
             "id": TestData.prolicense[0].id,
             "type": TestData.licTypes[1].name,
             "season": TestData.seasons[1].name,
@@ -178,7 +207,8 @@ describe("Конструктор пролицензий", () => {
     })
     test("Создание группы критериев пролицензии", async () => {
         for (const i of TestData.criteriaGroups) {
-            const response = await superagent.put(RequestProp.basicUrl + "/api/rest/prolicenses/" + TestData.prolicense[0].id + "/criterias/groupExperts").query(
+            const response = await superagent.put(RequestProp.basicUrl + "/api/rest/prolicenses/" + TestData.prolicense[0].id + "/criterias/groupExperts").
+            query(
                 {
                     groupId: i.id,
                     experts: [0, 1, 2]
@@ -207,7 +237,7 @@ describe("Конструктор пролицензий", () => {
                         "number": String(randomInt(1, 1000)),
                         "categoryId": TestData.rankCriteria[randomInt(0, TestData.rankCriteria.length - 1)].id,
                         "name": TestData.getRandomWord(),
-                        "description": TestData.getRandomWord(),
+                        "description": TestData.descValue,
                         "isMulti": randomInt(-1, 5),
                         "typeId": n.id,
                         "docSubmitDate": TestData.dateFuture,
@@ -264,12 +294,13 @@ describe("Конструктор пролицензий", () => {
     test("Изменение критериев пролицензии", async () => {
         for (const i of TestData.criteriasFullInfo) {
             for (const n of i.criterias) {
-                const response = await superagent.put(RequestProp.basicUrl + '/api/rest/prolicenses/criterias/' + n.id).send({
+                const response = await superagent.put(RequestProp.basicUrl + '/api/rest/prolicenses/criterias/' + n.id).
+                send({
                         "groupId": i.id,
                         "number": String(randomInt(1, 1000)),
                         "categoryId": TestData.rankCriteria[randomInt(0, TestData.rankCriteria.length - 1)].id,
                         "name": TestData.getRandomWord(),
-                        "description": TestData.getRandomWord(),
+                        "description": TestData.descValue,
                         "isMulti": randomInt(-1, 5),
                         "typeId": TestData.criteriaTypes[randomInt(0, TestData.criteriaTypes.length - 1)].id,
                         "docSubmitDate": TestData.dateFuture,
@@ -318,7 +349,8 @@ describe("Конструктор пролицензий", () => {
         })
     })
     test("Создание пролицензии по образцу", async () => {
-        const response = await superagent.put(RequestProp.basicUrl + "/api/rest/prolicenses/clone/" + TestData.prolicense[0].id).send({
+        const response = await superagent.put(RequestProp.basicUrl + "/api/rest/prolicenses/clone/" + TestData.prolicense[0].id).
+        send({
                 "type": TestData.licTypes[randomInt(0, TestData.licTypes.length - 1)].name,
                 "season": TestData.seasons[0].name,
                 "name": TestData.getRandomWord()
@@ -386,6 +418,181 @@ describe("Конструктор пролицензий", () => {
         expect(response.body.status).toBe("SUCCESS");
         expect(response.body.data).toEqual({});
         TestData.prolicense[0].stateId = 2;
+    })
+})
 
+describe("Работа с заявками", () => {
+    test("Создание заявки в статусе 'Черновик' ",async () => {
+        const response = await superagent.put(RequestProp.basicUrl+"/api/rest/licenses").
+        send(
+            {
+            "proLicId":TestData.prolicense[0].id,
+            "clubId":0
+            }
+            )
+        expect(response.ok).toBeTruthy();
+        expect(response.status).toBe(200);
+        expect(response.body.status).toBe("SUCCESS");
+        expect(response.body.data.proLicId).toBe(TestData.prolicense[0].id);
+        expect(response.body.data.state).toBe(TestData.getLicStatusById(response.body.data.stateId));
+        expect(response.body.data.percent).toBe(0);
+        expect(response.body.data.docState).toBe(TestData.getDocStatusById(response.body.data.docStateId));
+        TestData.license.push(response.body.data);
+        //Проверяем статусы документов лицензии
+        TestData.license[0].documents.forEach((value,index) => {
+            expect(value.state).toBe(TestData.getDocStatusById(response.body.data.docStateId));
+            expect(value.proDocId).toBe(TestData.prolicense[0].documents[index].id);
+        })
+        //Проверяем статусы и проценты заполнения групп критериев
+        TestData.license[0].criteriaGroups.forEach((value, index) => {
+            expect(value.groupId).toBe(TestData.criteriasFullInfo[index].id);
+            expect(value.state).toBe(TestData.getDocStatusById(value.stateId));
+            expect(value.percent).toBe(0.0);
+            //Проверяем статусы и проценты заполнения критериев
+            value.criterias.forEach((value1, index1) => {
+                expect(value1.state).toBe(TestData.getDocStatusById(value1.stateId));
+                expect(value1.percent).toBe(0.0);
+                //Проверяем статусы документов критериев
+                value1.documents.forEach(value2 => {
+                    expect(value2.state).toBe(TestData.getDocStatusById(value2.stateId));
+                })
+            })
+        })
+    })
+    test("Добавление документов и комментариев к документам вкладки 'Общая информация' ",async () => {
+        const response = await superagent.put(RequestProp.basicUrl+"/api/rest/licenses/"+TestData.license[0].id).
+            send({
+            "id": TestData.license[0].id,
+            "proLicId": TestData.license[0].proLicId,
+            "type": TestData.license[0].type,
+            "season": TestData.license[0].season,
+            "name": TestData.license[0].name,
+            "begin": TestData.license[0].begin,
+            "end": TestData.license[0].end,
+            "clubId": TestData.license[0].clubId,
+            "requestBegin": TestData.license[0].requestBegin,
+            "requestEnd": TestData.license[0].requestEnd,
+            "docSubmitDate": TestData.license[0].docSubmitDate,
+            "dueDate": TestData.license[0].dueDate,
+            "reviewDate": TestData.license[0].reviewDate,
+            "decisionDate": TestData.license[0].decisionDate,
+            "percent": TestData.license[0].percent,
+            "stateId": TestData.license[0].stateId,
+            "state": TestData.license[0].state,
+            "docStateId": TestData.license[0].docStateId,
+            "docState": TestData.license[0].docState,
+            "documents": [
+                {
+                    "id": TestData.license[0].documents[0].id,
+                    "proDocId": TestData.license[0].documents[0].proDocId,
+                    "name": TestData.license[0].documents[0].name,
+                    "docTypeId": TestData.license[0].documents[0].docTypeId,
+                    "comment": TestData.commentValue,
+                    "reviewComment": null,
+                    "stateId": TestData.license[0].documents[0].stateId,
+                    "state": TestData.license[0].documents[0].state,
+                    "templates": TestData.license[0].documents[0].templates,
+                    "files": [
+                        {
+                            "name": TestData.fileInfo[0][0],
+                            "storageId": TestData.fileInfo[0][1]
+                        },
+                        {
+                            "name": TestData.fileInfo[1][0],
+                            "storageId": TestData.fileInfo[1][1]
+                        },
+                        {
+                            "name": TestData.fileInfo[2][0],
+                            "storageId": TestData.fileInfo[2][1]
+                        }
+                    ]
+                },
+                {
+                    "id": TestData.license[0].documents[1].id,
+                    "proDocId": TestData.license[0].documents[1].proDocId,
+                    "name": TestData.license[0].documents[1].name,
+                    "docTypeId": TestData.license[0].documents[1].docTypeId,
+                    "comment": TestData.commentValue,
+                    "reviewComment": null,
+                    "stateId": TestData.license[0].documents[1].stateId,
+                    "state": TestData.license[0].documents[1].state,
+                    "templates": TestData.license[0].documents[1].templates,
+                    "files": [
+                        {
+                            "name": TestData.fileInfo[0][0],
+                            "storageId": TestData.fileInfo[0][1]
+                        },
+                        {
+                            "name": TestData.fileInfo[1][0],
+                            "storageId": TestData.fileInfo[1][1]
+                        },
+                        {
+                            "name": TestData.fileInfo[2][0],
+                            "storageId": TestData.fileInfo[2][1]
+                        }
+                    ]
+                }
+            ],
+            "criteriaGroups": TestData.license[0].criteriaGroups
+        })
+        expect(response.ok).toBeTruthy();
+        expect(response.status).toBe(200);
+        expect(response.body.status).toBe("SUCCESS");
+        TestData.license[0] = response.body.data;
+        //проверяем наличие добавленных документов и комментариев
+        TestData.license[0].documents.forEach((value, index) => {
+            expect(value.comment).toBeTruthy();
+            expect(value.files.length).toBeGreaterThan(0);
+        })
+    })
+    test("Добавление сотрудников клуба к группе критериев", async () => {
+            const response = await superagent.put(RequestProp.basicUrl+"/api/rest/licenses/"+TestData.license[0].id).
+            send({
+                "id": TestData.license[0].id,
+                "proLicId": TestData.license[0].proLicId,
+                "type": TestData.license[0].type,
+                "season": TestData.license[0].season,
+                "name": TestData.license[0].name,
+                "begin": TestData.license[0].begin,
+                "end": TestData.license[0].end,
+                "clubId": TestData.license[0].clubId,
+                "requestBegin": TestData.license[0].requestBegin,
+                "requestEnd": TestData.license[0].requestEnd,
+                "docSubmitDate": TestData.license[0].docSubmitDate,
+                "dueDate": TestData.license[0].dueDate,
+                "reviewDate": TestData.license[0].reviewDate,
+                "decisionDate": TestData.license[0].decisionDate,
+                "percent": TestData.license[0].percent,
+                "stateId": TestData.license[0].stateId,
+                "state": TestData.license[0].state,
+                "docStateId": TestData.license[0].docStateId,
+                "docState": TestData.license[0].docState,
+                "documents": TestData.license[0].documents,
+                "criteriaGroups": TestData.addClubWorkersToCritGrp()
+            })
+        expect(response.ok).toBeTruthy();
+        expect(response.status).toBe(200);
+        expect(response.body.status).toBe("SUCCESS");
+        //Проверяем наличие добавленных сотрудников клубов для групп критериев
+        TestData.license[0].criteriaGroups.forEach((value, index) => {
+            expect(value.experts).toEqual(response.body.data.criteriaGroups[index].experts);
+        })
+    })
+    test("Добавление документов и комментариев для документов критериев", async () => {
+        const response = await superagent.put(RequestProp.basicUrl+"/api/rest/licenses/"+TestData.license[0].id).
+        send(TestData.addDocAndComToCritDoc());
+        expect(response.ok).toBeTruthy();
+        expect(response.status).toBe(200);
+        expect(response.body.status).toBe("SUCCESS");
+        //Проверяем наличие добавленных комментариев и документов для документов критериев
+        TestData.license[0].criteriaGroups.forEach((value,index) => {
+            value.criterias.forEach((value1,index1) => {
+                value1.documents.forEach((value2, index2) => {
+                    const addedDocInfo = response.body.data.criteriaGroups[index].criterias[index1].documents[index2];
+                    expect(value2.comment).toBe(addedDocInfo.comment);
+                    expect(value2.files.length).toBe(addedDocInfo.files.length);
+                })
+            })
+        })
     })
 })
