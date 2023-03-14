9a8380239c1e8aa83e5cbf3ee6fb95cf64062129
Thefaceless1 <zrjdif93@gmail.com>
2023-03-14 10:54:05 +0300

    added new changes

--- src/e2e/tests/request/request.page.spec.ts
+++ src/e2e/tests/request/request.page.spec.ts
@@ -1,7 +1,7 @@
 import {expect} from "@playwright/test";
 import {test} from "../../page-objects/helpers/fixtures/fixtures.js";
 
-test.describe.only("Work with requests", () => {
+test.describe("Work with requests", () => {
     test("Work with requests scenario",async ({requests},testInfo) => {
         await test.step("Filling in experts and club members for criteria groups",async () => await requests.addExperts());
         await test.step("Filling criteria documents, club workers and ofi",async () => await requests.addCritInfo());
